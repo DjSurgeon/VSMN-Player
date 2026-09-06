b3ef2ffbcc6a4ed017dfb8fcbc204ca267c112f8
DJSurgeon <djsurgeon83@gmail.com>
2026-09-06 19:22:24 +0200

    chore(conan): Create conan configuration file

---
+++ conanfile.py
@@ -0,0 +1,15 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class IptvPlayerConan(ConanFile):
+    name = "iptv_player"
+    version = "0.1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires("spdlog/1.13.0")
+        self.requires("gtest/1.14.0")
