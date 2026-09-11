d9b118beebc4297d0070d428e400acd30a2e82d9
DJSurgeon <djsurgeon83@gmail.com>
2026-09-11 18:27:05 +0200

    fix(client): clang-style

--- src/network/http_curl_callbacks.cpp
+++ src/network/http_curl_callbacks.cpp
@@ -1,8 +1,9 @@
 #include "iptv/network/http_curl_callbacks.hpp"
-#include "iptv/network/http_response.hpp"
 
 #include <charconv>
 #include <string_view>
+
+#include "iptv/network/http_response.hpp"
 
 namespace iptv::network::detail {
 
@@ -13,18 +14,21 @@ size_t writeCallback(char* ptr, size_t size, size_t nmemb, void* userdata) {
 
   auto* response = static_cast<HttpResponse*>(userdata);
   std::size_t total_size = size * nmemb;
-  response->appendToBody(static_cast<const uint8_t*>(static_cast<const void*>(ptr)), total_size);
+  response->appendToBody(
+      static_cast<const uint8_t*>(static_cast<const void*>(ptr)), total_size);
   return total_size;
 }
 
-size_t headerCallback(char* buffer, size_t size, size_t nitems, void* userdata) {
+size_t headerCallback(char* buffer, size_t size, size_t nitems,
+                      void* userdata) {
   if (userdata == nullptr) {
     return size * nitems;
   }
   size_t total = size * nitems;
   std::string_view line(buffer, total);
 
-  if (line.starts_with("Content-Length:") || line.starts_with("content-length:")) {
+  if (line.starts_with("Content-Length:") ||
+      line.starts_with("content-length:")) {
     size_t pos = line.find(':');
     if (pos != std::string_view::npos) {
       std::string_view val = line.substr(pos + 1);
@@ -32,7 +36,8 @@ size_t headerCallback(char* buffer, size_t size, size_t nitems, void* userdata) 
       if (first != std::string_view::npos) {
         val = val.substr(first);
         size_t content_length = 0;
-        auto [p, ec] = std::from_chars(val.data(), val.data() + val.size(), content_length);
+        auto [p, ec] = std::from_chars(val.data(), val.data() + val.size(),
+                                       content_length);
         if (ec == std::errc{}) {
           auto* response = static_cast<HttpResponse*>(userdata);
           response->reserveBody(content_length);
